facd595ccf9f927ba80464b64fbfc9f33dec6fbe
Daoud Houssemeddine <houssemdaou85@yahoo.com>
2023-06-19 19:49:34 +0100

    send email response

--- src/main/kotlin/com/programasoft/application/registrationrequest/RegistrationRequestController.kt
+++ src/main/kotlin/com/programasoft/application/registrationrequest/RegistrationRequestController.kt
@@ -3,6 +3,8 @@ package com.programasoft.application.registrationrequest
 import com.programasoft.application.account.AccountService
 import com.programasoft.application.certificate.CertificateService
 import org.springframework.http.HttpStatus
+import org.springframework.mail.SimpleMailMessage
+import org.springframework.mail.javamail.JavaMailSender
 import org.springframework.web.bind.annotation.*
 import org.springframework.web.server.ResponseStatusException
 import java.util.*
@@ -13,7 +15,8 @@ import java.util.*
 class RegistrationRequestController(
     private val registrationRequestService: RegistrationRequestService,
     private val certificateService: CertificateService,
-    private val accountService: AccountService
+    private val accountService: AccountService,
+    private val mainSender: JavaMailSender
 ) {
 
     @PostMapping
@@ -55,13 +58,75 @@ class RegistrationRequestController(
     @PutMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
     fun updateRegistrationRequestStatus(
-        @PathVariable id: Long,
-        @RequestParam status: RegistrationStatus
+        @PathVariable id: Long, @RequestParam status: RegistrationStatus
     ): RegistrationRequest {
         val registrationRequest = registrationRequestService.getById(id)
-        return registrationRequestService.create(registrationRequest.copy(
-            status = status,
-            dateOfResponse = Date()
-        ))
+        val updatedRegistrationRequest = registrationRequestService.create(
+            registrationRequest.copy(
+                status = status, dateOfResponse = Date()
+            )
+        )
+        when (status) {
+            RegistrationStatus.PENDING -> {
+
+            }
+
+            RegistrationStatus.APPROVED -> {
+                val subject = "Registration Request Accepted"
+                val body = generatePositiveResponseEmail(
+                    "http://localhost:3000/create-psychologist/${updatedRegistrationRequest.id}"
+                )
+                sendEmail(updatedRegistrationRequest.email, subject, body, mainSender)
+            }
+
+            RegistrationStatus.REJECTED -> {
+                val subject = "Registration Request Rejected"
+                val body = generateNegativeResponseEmail(
+                )
+                sendEmail(updatedRegistrationRequest.email, subject, body, mainSender)
+            }
+        }
+        return updatedRegistrationRequest
+    }
+
+    fun sendEmail(to: String, subject: String, body: String, mailSender: JavaMailSender) {
+        val message = SimpleMailMessage().apply {
+            setTo(to)
+            setSubject(subject)
+            setText(body)
+        }
+
+        mailSender.send(message)
+    }
+
+    fun generateNegativeResponseEmail(): String {
+        val subject = "Registration Request Rejected"
+        val body = """
+        Dear Psychologist,
+        
+        We regret to inform you that your registration request has been rejected. If you have any questions or would like to obtain further information, please feel free to contact us.
+        
+        Thank you for your interest.
+        
+        Best regards,
+        Administrator
+    """.trimIndent()
+
+        return body
+    }
+
+    fun generatePositiveResponseEmail(accountCreationLink: String): String {
+        val body = """
+        Dear Psychologist,
+        
+        Your registration request has been accepted. You can now create your account using the following link: $accountCreationLink
+        
+        Thank you and welcome to our online psychological consultation platform.
+        
+        Best regards,
+        Administrator
+    """.trimIndent()
+
+        return body
     }
 }
